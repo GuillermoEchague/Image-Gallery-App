432461fd5e2348e3839e4bd5bf711d71444c8a4a
Guillermo Echague <46538216+GuillermoEchague@users.noreply.github.com>
2024-12-09 22:53:44 -0300

    Initial Commit

---
+++ GaleriaImagenes/GaleriaImagenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaleriaImagenesApp.swift
+//  GaleriaImagenes
+//
+//  Created by Guillermo Echague on 09-12-24.
+//
+
+import SwiftUI
+
+@main
+struct GaleriaImagenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
